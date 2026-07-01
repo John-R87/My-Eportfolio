fb570e7d319debb01eae6d7ee2d7bfdfcbea49d9
John-R87 <johnathan.rosales@outlook.com>
2026-07-01 13:18:03 -0700

    javascript for email

---
+++ index.js
@@ -0,0 +1,15 @@
+// template_7eya3or 
+// service_m8fwl1h
+// dPyp903lgvh4LXCQD
+//
+
+function contact(event) {
+   event.preventDefault();
+   emailjs
+   .sendForm(
+      'service_m8fwl1h',
+      'template_7eya3or',
+      event.target,
+      'dPyp903lgvh4LXCQD'
+   )
+}
