ec282a4e3fbe6dd1034d9de640dd59bf34019319
John-R87 <johnathan.rosales@outlook.com>
2026-07-01 15:44:46 -0700

    modal changes

--- index.js
+++ index.js
@@ -5,11 +5,39 @@
 
 function contact(event) {
    event.preventDefault();
+   const loading = document.querySelector('.modal__overlay--loading');
+   const success = document.querySelector('.modal__overlay--success');
+   loading.classList += " modal__overlay--visible";
+
    emailjs
    .sendForm(
       'service_m8fwl1h',
       'template_7eya3or',
       event.target,
       'dPyp903lgvh4LXCQD'
-   )
+   ).then(() => {
+     loading.classList.remove("modal__overlay--visible");
+     success.classList += " modal__overlay--visible";
+}).catch(() => {
+   loading.classList.remove("modal__overlay--visible");
+   alert(
+      "The email service is temporarily unavailable. Please contact me directly at johnathan.rosales@outlook.com"
+   );
+})
+
+setTimeout(() => {
+
+   console.log('this worked 1')
+}, 1000);
+}
+
+
+let isModalOpen = false;
+function toggleModal() {
+   if (isModalOpen) {
+      isModalOpen = false;
+      return document.body.classList.remove("modal--open");
+   }
+   isModalOpen = true;
+   document.body.classList += " modal--open";
 }
